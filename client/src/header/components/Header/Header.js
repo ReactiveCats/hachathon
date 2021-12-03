c0b595aa0de67f63a62d3559c3d9c7cbcb619c45
denisSorokinZ <d.sorokin@gmail.com>
2021-12-03 23:29:59 +0200

    navbar 2

--- client/src/header/components/Header/Header.js
+++ client/src/header/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   AppBar,
   Toolbar,
@@ -5,35 +6,74 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  Button,
+  Grid,
 } from '@mui/material';
 
 function Header() {
+  const [view, setView] = useState(0);
+  const [filter, setFilter] = useState(0);
+
+  const handleViewChange = (event) => {
+    setView(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
-    <header>
-      {/* Dropdown */}
-      <AppBar position="static">
-        <Toolbar>
-          <FormControl>
-            <InputLabel id="view-label">View</InputLabel>
-            <Select
-              labelId="view-label"
-              id="view-select"
-              value={0}
-              label="View"
-              // onChange={handleChange}
-            >
-              <MenuItem value={0}>Table</MenuItem>
-              <MenuItem value={1}>List</MenuItem>
-            </Select>
-          </FormControl>
-        </Toolbar>
-      </AppBar>
+    <AppBar position="static">
+      <Toolbar>
+        <Grid
+          container
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          paddingY="16px"
+          width="100%"
+        >
+          {/* Views */}
+          <Grid item>
+            <FormControl>
+              <InputLabel id="view-label">View</InputLabel>
+              <Select
+                labelId="view-label"
+                id="view-select"
+                value={view}
+                label="View"
+                onChange={handleViewChange}
+              >
+                <MenuItem value={0}>Table</MenuItem>
+                <MenuItem value={1}>List</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
 
-      {/* Filters */}
+          {/* Filters */}
+          <Grid item>
+            <FormControl>
+              <InputLabel id="filter-label">Filters</InputLabel>
+              <Select
+                labelId="filter-label"
+                id="filter-select"
+                value={filter}
+                label="Filter"
+                onChange={handleFilterChange}
+              >
+                <MenuItem value={0}>Filter 1</MenuItem>
+                <MenuItem value={1}>Filter 2</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
 
-      {/* Add */}
-      {/* dispatch(action: open) */}
-    </header>
+          {/* Add Task Button */}
+          <Grid item>
+            <Button variant="contained">+</Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 }
 
