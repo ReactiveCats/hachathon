c90340fe2bae8f71f433a8bd5f23b2d9e49d1533
Stanislav Strelnikov <72222779+stasstrelnikov@users.noreply.github.com>
2021-12-03 21:21:50 +0300

    feat: material ui

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,23 @@
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import { theme } from '../shared/theme';
+import { createEmotionCache } from '../shared/create-emotion-cache';
+
 function HackathonApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CacheProvider value={createEmotionCache()}>
+      <Head>
+        <title>Hackathon</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
 export default HackathonApp;
--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -1,4 +1,8 @@
+import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import { createEmotionCache } from '../shared/create-emotion-cache';
+import { theme } from '../shared/theme';
 
 class HackathonDocument extends Document {
   static async getInitialProps(ctx) {
@@ -9,7 +13,13 @@ class HackathonDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
@@ -18,5 +28,39 @@ class HackathonDocument extends Document {
     );
   }
 }
+
+HackathonDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
+  };
+};
 
 export default HackathonDocument;
