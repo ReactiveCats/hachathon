b2ba83ba927eb5a09adf4378dcccb4656e880015
denisSorokinZ <d.sorokin@gmail.com>
2021-12-04 20:21:56 +0200

    finished navbar

--- client/src/shared/theme.js
+++ client/src/shared/theme.js
@@ -1,3 +1,8 @@
 import { createTheme } from '@mui/material/styles';
 
-export const theme = createTheme({ palette: { gray: { main: '#808080' } } });
+export const theme = createTheme({
+  palette: {
+    gray: { main: '#808080' },
+    navbarBackground: { main: 'rgba(75, 125, 255, 0.2)' },
+  },
+});
