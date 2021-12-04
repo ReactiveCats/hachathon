a1f11f5d294cc104db639e31240324ebbc81a8ca
Stanislav Strelnikov <72222779+stasstrelnikov@users.noreply.github.com>
2021-12-04 14:27:49 +0300

    feat(client/auth): add guard

---
+++ client/src/auth/guards/auth-guard.js
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useAuthContext } from '../context/auth-context';
+
+export function AuthGuard({ children }) {
+  const router = useRouter();
+
+  const { state, dispatch } = useAuthContext();
+
+  useEffect(() => {
+    if (!state.authorized) {
+      router.push({
+        pathname: '/auth',
+        query: { from: router.asPath },
+      });
+    }
+  }, []);
+
+  return state.authorized ? children : <></>;
+}
