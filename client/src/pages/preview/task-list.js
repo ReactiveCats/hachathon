85dbeea6b56d8dd8085ceea94729d98f28f0220d
Stanislav Strelnikov <72222779+stasstrelnikov@users.noreply.github.com>
2021-12-04 17:56:31 +0300

    feat(client/task-list): add ui

--- client/src/pages/preview/task-list.js
+++ client/src/pages/preview/task-list.js
@@ -1,9 +1,15 @@
+import { Container, Typography } from '@mui/material';
 import { TaskList } from '../../task-list/components/task-list-component';
 
 function TaskListPreviewPage() {
   return (
     <>
-      <TaskList></TaskList>
+      <Container sx={{ paddingY: 4 }}>
+        <Typography variant="h4" component="h1">
+          Your tasks
+        </Typography>
+        <TaskList></TaskList>
+      </Container>
     </>
   );
 }
--- client/src/task-modal/components/task-modal-component.js
+++ client/src/task-modal/components/task-modal-component.js
@@ -1,6 +1,7 @@
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import isEqual from '@tinkoff/utils/is/equal';
+import { theme } from '../../shared/theme';
 import {
   TASK_MODAL_HIDE,
   TASK_MODAL_SAVE,
@@ -12,12 +13,20 @@ const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
+  minWidth: 286,
   transform: 'translate(-50%, -50%)',
-  width: 400,
   bgcolor: 'background.paper',
   boxShadow: 24,
   borderRadius: 2,
   p: 4,
+
+  [theme.breakpoints.up('md')]: {
+    width: 432,
+  },
+
+  [theme.breakpoints.up('lg')]: {
+    width: 574,
+  },
 };
 
 const taskFromData = (data) => ({
