a07bad55c93837efb43cab474c2d4a22c61b37dd
IDSAD <idsad.ch@gmail.com>
2021-12-04 22:05:48 +0200

    added: top card time

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,15 @@
 import Navbar from "../navbar/components/navbar";
+import TopCardComponent from '../top-card/components/top-card-component';
+import { TaskList } from '../task-list/components/task-list-component';
 
 function Home() {
   return (
     <>
       <Navbar name="Name" />
-
-      <main></main>
+      <TopCardComponent />
+      <main>
+        <TaskList />
+      </main>
 
       <footer></footer>
     </>
--- client/src/top-card/components/top-card-component.js
+++ client/src/top-card/components/top-card-component.js
@@ -1,19 +1,27 @@
 import { Box, Container, Grid, Typography } from '@mui/material';
 import tasks from '../../shared/mock-tasks.json';
 import icons from '../../shared/icons';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import { convertTime } from '../../shared/convert-time';
+import { useEffect } from 'react';
 
-const task = tasks[0];
+const task = tasks[1];
 const Icon = icons[task.icon];
 
 /* Styles */
 
-const boxStyles = {
+const boxStyles = (theme) =>  ({
   background: theme => theme.palette.gradientBlue.main,
-  height: '375px'
-}
+  minHeight: '265px',
+  boxShadow: 10,
+  [theme.breakpoints.up('sm')]: {
+    minHeight: '350px'
+  }
+})
 
 const iconStyle = (theme) => ({
   fontSize: 70,
+  maxWidth: '100%',
   [theme.breakpoints.up('sm')]: {
     fontSize: 215
   }
@@ -21,8 +29,37 @@ const iconStyle = (theme) => ({
 
 const containerStyle = {
   height: '100%',
-  paddingTop: '100px'
+  paddingTop: '100px',
 }
+
+const titleStyle = (theme) => ({
+  marginBottom: '10px',
+  [theme.breakpoints.down('sm')]: {
+    fontSize: 34
+  }
+})
+
+const descriptionDesktopStyle = (theme) => ({
+  [theme.breakpoints.down('sm')]: {
+    display: 'none'
+  }
+})
+
+const descriptionMobileStyle = (theme) => ({
+  display: 'none',
+  paddingBottom: '20px',
+  [theme.breakpoints.down('sm')]: {
+    display: 'block'
+  }
+})
+
+const estimationStyle = (theme) => ({
+  marginTop: '15px',
+  display: 'flex',
+  [theme.breakpoints.down('sm')]: {
+    marginTop: 0,
+  }
+})
 
 const TopCardComponent = () => {
   return (
@@ -36,14 +73,23 @@ const TopCardComponent = () => {
           <Grid item xs={3}>
             <Icon color='white' sx={iconStyle} />
           </Grid>
-          <Grid item xs={9}>
-            <Typography variant="h3" color={theme => theme.palette.white.main}>
+          <Grid item container xs={9} justifyContent="center" flexDirection="column">
+            <Typography variant="h3" color={theme => theme.palette.white.main} sx={titleStyle}>
               {tasks[0].title}
             </Typography>
+            <Typography variant="body1" color={theme => theme.palette.white.main} sx={descriptionDesktopStyle}>
+              {tasks[0].description}
+            </Typography>
+            {tasks[0].estimated &&
+              <Typography variant="body1" color={theme => theme.palette.white.main} sx={estimationStyle}>
+                <AccessTimeIcon color='white' sx={{marginRight: 1}} /> {convertTime(parseInt(tasks[0].estimated))}
+              </Typography>
+            }
+          </Grid>
+          <Grid item sx={descriptionMobileStyle} mt={2}>
             <Typography variant="body1" color={theme => theme.palette.white.main}>
               {tasks[0].description}
             </Typography>
-
           </Grid>
         </Grid>
       </Container>
